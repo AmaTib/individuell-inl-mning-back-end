44bb3024c57107873efe4e3a3500a9f596ef02c2
Amanda Tibell <141629199+AmaTib@users.noreply.github.com>
2024-01-26 21:46:18 +0100

    added more players to the array

--- players.js
+++ players.js
@@ -29,36 +29,37 @@ const players = [
     position: "Left Wing/Forward",
     id: "5",
   },
-  /*     {
-        name:"Henrik Lundqvist"
-    jersey:
-    position:
-    id:
-}
-    {
-        name:"Henrik Sedin"
-    jersey:
-    position:
-    id:
-}
-    {
-        name:"Henrik Zetterberg"
-    jersey:
-    position:
-    id:
-}
-    {
-        name:"Markus Naslund"
-    jersey:
-    position:
-    id:
-}
-    {
-        name:"Mats Sundin"
-    jersey:
-    position:
-    id:
-    }*/
+  {
+    name: "Henrik Lundqvist",
+    jersey: "30",
+    position: "Goalkeeper",
+    id: "6",
+  },
+
+  {
+    name: "Henrik Sedin",
+    jersey: "30",
+    position: "Center/Forward",
+    id: "7",
+  },
+  {
+    name: "Henrik Zetterberg",
+    jersey: "40",
+    position: "Left Wing/Center",
+    id: "8",
+  },
+  {
+    name: "Markus Näslund",
+    jersey: "19",
+    position: "Left Wing",
+    id: "9",
+  },
+  {
+    name: "Mats Sundin",
+    jersey: "13",
+    position: "Center",
+    id: "10",
+  },
 ];
 
 module.exports.players = players;
