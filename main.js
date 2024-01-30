39cb811c931082561d28305ae4d5a8f3c62bffd7
Amanda Tibell <141629199+AmaTib@users.noreply.github.com>
2024-01-30 12:52:34 +0100

    added get request to find player with id

--- main.js
+++ main.js
@@ -10,11 +10,24 @@ var bodyParser = require("body-parser"); //för att rest-client verktyget ska fu
 app.use(bodyParser.json());
 app.use(cors());
 
+//REQUESTS
+//get all players
 app.get("/players", (req, res) => {
   res.json(players);
   console.log(players);
 });
 
+//get players with specific id
+app.get("/players/:id", (req, res) => {
+  let onePlayer = players.find((player) => player.id === req.params.id);
+  if (onePlayer === undefined) {
+    res.status(404).send("Finns inte");
+  }
+  res.json(onePlayer);
+  console.log(onePlayer);
+});
+
+//creates new player and pushes to "players" array
 app.post("/players", (req, res) => {
   const player = {
     name: req.body.name,
